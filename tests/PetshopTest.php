8c5490781a19f87a04f37807ba1d41054540683d
Eltonmc <eltonmc@dcomp.ufs.br>
2019-05-21 23:26:40 -0300

    add get all petshops test

--- tests/PetshopTest.php
+++ tests/PetshopTest.php
@@ -34,13 +34,44 @@ class PetshopTest extends TestCase
         return ['Authorization' => "Bearer $json->token"];
     }
 
-
     public function test_can_create_a_petshop(){
         $petshop = factory('App\Petshop')->make();
         $response = $this->post("petshops", $petshop->toArray(), $this->get_header());
         // dd($this->response->getContent());
         $this->seeStatusCode(200);
         $this->seeJsonStructure($this->petshop_json);
+    }
+
+    public function test_can_get_all_petshops(){
+        $petshop = factory('App\Petshop')->create();
+        $this->get('/petshops', $this->get_header());
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'data' => ['*' =>
+                [
+                    'id',
+                    'user_id',
+                    'name',
+                    'description',
+                    'phone',
+                    'address',
+                    'images',
+                    'created_at',
+                    'updated_at',
+                ]
+            ],
+            'meta' => [
+                '*' => [
+                    'total',
+                    'count',
+                    'per_page',
+                    'current_page',
+                    'total_pages',
+                    'links',
+                ]
+            ]
+        ]);
+        // $this->seeJsonStructure($this->petshop_json);
     }
 
     public function test_can_get_petshop_info(){
