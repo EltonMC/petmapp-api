d6d548e1f1ea11d1102f96314a024cb22bbf7f24
Leandro Souza <jlssouza@dcomp.ufs.br>
2019-06-01 11:31:26 -0300

    fix seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
-        factory(App\Product::class, 10)->create();
+        //factory(App\Product::class, 10)->create();
         Model::unguard();
         // Register the user seeder
         // factory(App\User::class, 10)->create();
